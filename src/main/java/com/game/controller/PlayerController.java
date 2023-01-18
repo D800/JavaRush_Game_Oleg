bca7700f8734c222498f3d9cdce6f3e58df3dbc4
Dima <dima.myagkov1@yandex.ru>
2023-01-18 11:59:01 +0300

    Начало, настроеные исходники тестового задания.

---
+++ src/main/java/com/game/controller/PlayerController.java
@@ -0,0 +1,19 @@
+package com.game.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ulbi_users")
+public class PlayerController {
+    @GetMapping
+    public ResponseEntity getUsers() {
+        try {
+            return ResponseEntity.ok("Сервер работает");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+}
